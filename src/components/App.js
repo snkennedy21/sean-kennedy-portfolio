0dbf3753dab430a9db6039e3c15b7d506d34a01c
Sean Kennedy <snkennedy21@gmail.com>
2022-11-10 09:08:00 -0500

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import Header from "./header/Header";
+import Hero from "./hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Hero></Hero>
     </div>
   );
 }
---
+++ src/components/hero/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="w-full h-screen bg-blue-500 -mt-20">
+      <h1 className="text-7xl -mt-">Sean Kennedy</h1>
+    </section>
+  );
+}
+
+export default Hero;
